5974a19f5730f31a8b3285e23901e3eed595e3ac
आपका नाम < >
2025-01-05 23:49:19 +0530

    Initial commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,11 +6,22 @@ const USER = mongoose.model("user");
 
 router.get("/user/:id", (req, res) => {
     USER.findOne({ _id: req.params.id })
-    .select("-password")
-    .then(user => {
-        res.json( user )
-    })
-        
+        .select("-password")
+        .then(user => {
+            POST.find({ postedBy: req.params.id })
+                .populate("postedBy", "_id")
+                .then((posts) => {
+                    res.status(200).json({ user, posts });
+                })
+                .catch((err) => {
+                    res.status(422).json({ error: err });
+                });
+                
+        })
+        .catch(err => {
+            return res.status(404).json({ error: "User not found" });
+        });
+
 })
 
 module.exports = router;
